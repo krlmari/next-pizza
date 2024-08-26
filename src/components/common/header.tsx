176bb898fef7e7f5f9957e10ec3afaf853f70fb0
krlmari <krylova_m_v@inbox.ru>
2024-08-27 00:08:51 +0200

    feat: edit next.config

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -14,7 +14,7 @@ export const Header: React.FC<Props> = ({ className }) => {
     <div className={cn("border-b", className)}>
       <Container className="flex items-center justify-between py-8">
         <div className="flex items-center gap-4">
-          <Image src="/logo.png" alt="Logo" width={35} height={35} />
+          <Image src="/next-pizza/logo.png" alt="Logo" width={35} height={35} />
           <div>
             <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
             <p className="text-sm text-gray-400 leading-3">
